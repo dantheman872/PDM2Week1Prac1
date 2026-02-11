2cfcac9b38f09d973b44b42acb55fc27dff33ab4
dantheman872 <95766593+dantheman872@users.noreply.github.com>
2026-02-11 12:40:19 +0000

    Update sketch.js

--- 3star/sketch.js
+++ 3star/sketch.js
@@ -1,37 +1,132 @@
+let cols = 3; let rows = 3;
+let size;
+let board = [];
+let players = ["X", "O"]
+let currentPlayer;
+let winner = false
+let winnerLoc = [];
+let winnerText;
+
 function setup(){
 
-    createCanvas(300,300)
-
-}
-
-let cols = 3
-let rows = 3
-let box1, box2
-
-function draw(){
-
+    createCanvas(400, 400)
+    size = width/cols;
+    currentPlayer = players[floor(random(2))];
     for(let i = 0; i < cols; i++){
 
-        line(i * 100, 0, i * 100, height)
-        line(0, i * 100, width, i * 100)
-        box1 = i * 50
-        box2 = i * 50
-        if(isMouseOver(box1, box2)){
+        board[i] = [];
+        for(let j = 0; j < rows; j++){
 
-            fill(200)
+            board[i][j] = 0;
         }
     }
 }
 
-function drawLetter(){
+function draw(){
 
+    background(220);
+    drawBoard()
+    drawWinner()
 }
 
-function isMouseOver(boxX, boxY){
+function drawBoard(){
 
-    if(mouseX > boxX - 50 && mouseX < boxX + 50 
-    && mouseY > boxY - 50 && mouseY < boxY + 50){
+    stroke(0)
+    strokeWeight(1)
+    for(let i = 0; i < cols; i++){
 
-        return true
+        for(let j = 0; j < rows; j++){
+
+            rect(i * size, j * size, size, size)
+
+            if(board[i][j] != 0){
+
+                textAlign(CENTER)
+                textSize(30)
+                text(board[i][j], size/2 + i * size, size/2 + j * size + 10)
+            }
+        }
+    }
+}
+
+function mousePressed(){
+
+    let index = [floor(mouseX / size), floor(mouseY / size)];
+    placePieces(index[0], index[1]);
+    checkWinner()
+}
+
+function placePieces(x, y){
+
+    if(board[x][y] == 0){
+
+        board[x][y] = currentPlayer;
+        winnerText = currentPlayer;
+        if(currentPlayer == "X"){
+
+            currentPlayer = "O"
+        } else {
+
+            currentPlayer = "X"
+        }
+    } 
+}
+
+function checkWinner(){
+
+    for(let i = 0; i < cols; i++){
+
+        if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != 0){
+
+            winner = true;
+            winnerLoc = [1, i]
+        }
+    }
+
+    for(let i = 0; i < rows; i++){
+
+        if(board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != 0){
+
+            winner = true;
+            winnerLoc = [2, i]
+        }
+    }
+
+    if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != 0){
+
+        winner = true
+        winnerLoc = [3, 0]
+    }
+
+    if(board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[2][0] != 0){
+
+        winner = true
+        winnerLoc = [4, 0]
+    }
+    print(winner);
+}
+
+function drawWinner(){
+
+    stroke(255, 0, 0)
+    strokeWeight(5)
+    if(winner){
+
+        textAlign(CENTER)
+        textSize(100)
+        text(winnerText + " wins", width/2, height/2 + 0)
+        if(winnerLoc[0] == 1){
+
+            line(size/2 + winnerLoc[1] * size, size /2 + 0 * size, size/2 + winnerLoc[1] * size, size/2 + 2*size)
+        } else if (winnerLoc[0] == 2){
+
+            line(size/2 + 0 * size, size/2 + winnerLoc[1] * size, size/2 + 2 * size, size/2 + winnerLoc[1] * size)
+        } else if (winnerLoc[0] == 3){
+
+            line(size/2, size/2, size/2 + 2 * size, size/2 + 2 * size)
+        } else if (winnerLoc[0] == 4){
+
+            line(size/2 + 2 * size, size/2, size/2, size/2 + 2 * size)
+        }
     }
 }
